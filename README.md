18fc27d0092296cbbde5bbfbae52765e0e555ef4
Scott Newman <snewman18@gmail.com>
2022-08-24 14:47:19 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # featherwing-random-mp3-player
 A shuffling MP3 player using the Adafruit FeatherWing Music Maker
+
+I have a project in mind that will play music files with no interface, just a power switch. I
+used an (https://www.adafruit.com/product/3357)[Adafruit Music Maker FeatherWing] on a Feather Board to
+implement the MP3 player portion.
+
+This code will read all the MP3 files in the *root* directory of the SD card and continuously play them in random
+order. It will skip over directories and hidden files. (like the `.Spotlight-V100` MacOS puts on the card)
+
+Because of memory limitations, it's not possible to read all the names of the files into an array and sample from it.
+Instead, I count the number of MP3 files on the SD card, then I choose a random number between `0` and `count` to
+pick a song to play. There is no checking to see if the file has been recently played, that would be a nice feature
+to add. (Pull requests gladly accepted!)
+
+Limitations:
+* The MP3 files must be in the root of the SD card, they cannot be in directories
+* The MP3 filenames must be in 8.3 filename format (8 or less characters)
+* The MP3 filename must end with a lowercase `.mp3` extension
+* The songs are chosen randomly so it's possible to play the same song repeatedly
---
+++ Random_MP3_Player.ino
@@ -0,0 +1,123 @@
+// Specifically for use with the Adafruit Feather, the pins are pre-set here!
+
+// include SPI, MP3 and SD libraries
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_VS1053.h>
+
+// These are the pins used for the Feather ESP8266
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+#define VS1053_CS      16     // VS1053 chip select pin (output)
+#define VS1053_DCS     15     // VS1053 Data/command select pin (output)
+#define CARDCS          2     // Card chip select pin
+#define VS1053_DREQ     0     // VS1053 Data request, ideally an Interrupt pin
+
+Adafruit_VS1053_FilePlayer musicPlayer =
+  Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
+
+File sdCardRoot;
+int numberOfMp3Files = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  if (!musicPlayer.begin()) { // initialise the music player
+    Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+    while (1);
+  }
+
+  musicPlayer.setVolume(0, 0);
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
+
+  if (!SD.begin(CARDCS)) {
+    Serial.println(F("SD failed, or not present"));
+    while (1);  // don't do anything more
+  }
+
+  sdCardRoot = SD.open("/");
+  numberOfMp3Files = countMp3Files(sdCardRoot);
+  Serial.println("Found " + String(numberOfMp3Files) + " MP3 files");
+
+  playRandomMp3File();
+}
+
+void loop() {  
+  if (musicPlayer.stopped()) {
+    Serial.println("Music player is stopped.");
+    playRandomMp3File();
+  }
+
+  delay(100);
+}
+
+bool isValidMp3FileName(String fileName) {
+  bool endsWithMp3Suffix = fileName.endsWith(String(".mp3"));
+  bool isNotHiddenFile = !fileName.startsWith(String("."));
+  return isNotHiddenFile && endsWithMp3Suffix;
+}
+
+int countMp3Files(File dir) {
+  int numberOfMp3Files = 0;
+  String fileName;
+  
+  while(true) {     
+    File entry =  dir.openNextFile();
+
+    if (!entry) {
+      break;
+    }
+    else {
+      fileName = String(entry.name());
+    }
+
+    if (!entry.isDirectory() && isValidMp3FileName(fileName)) {
+      numberOfMp3Files++;
+    }
+     
+    entry.close();
+  }
+
+  dir.rewindDirectory();
+  return numberOfMp3Files;
+}
+
+String getMp3FilenameByIndexNumber(int targetIndex, File dir) {
+  int mp3FileIndex = -1;
+  String fileName;
+
+  while (true) {
+    File entry =  dir.openNextFile();
+    
+    if (!entry) {
+      break;
+    }
+    else {
+      fileName = String(entry.name());
+    }
+    
+    if (!entry.isDirectory() && isValidMp3FileName(fileName)) {
+      mp3FileIndex++;
+    }
+
+    if (mp3FileIndex == targetIndex) {
+      String filePath = "/" + String(entry.name());
+      entry.close();
+      return filePath;
+    }
+    else {
+      entry.close();
+    }
+  }
+}
+
+void playRandomMp3File() {
+    int randomFileNumber = random(0, numberOfMp3Files);
+    String filename = getMp3FilenameByIndexNumber(randomFileNumber, sdCardRoot);
+
+    // You can't play a string name, it must be converted to char first
+    char charBuf[50];
+    filename.toCharArray(charBuf, 50);
+
+    Serial.println("Playing file: " + filename);    
+    musicPlayer.startPlayingFile(charBuf);
+}
